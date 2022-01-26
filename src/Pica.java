a3648d1d7a3fe224e97da8fc41d13d73be2cfe8d
Gints <gints.kanders@inbox.lv>
2022-01-26 09:27:55 +0200

    Programas 1.daļā

--- src/Pica.java
+++ src/Pica.java
@@ -1,9 +1,49 @@
+import java.util.Scanner;
 
 public class Pica {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		fikset();
+		
+		
+		
+		
+		
+		
+		
 	}
+		
+		public static void fikset() {
+			Scanner scan = new Scanner(System.in);
+			String v,u,a = null;
+			int t;
+			char n=0;
+			
+			
+			System.out.println("Adrese: ");
+			a = scan.nextLine();
+			for(int i=0;i<a.length();i++) {
+				if(Character.isDigit(a.charAt(i))) {
+					n=a.charAt(i);
+					
+			System.out.println("vārds: ");
+			v = scan.next();
+			System.out.println("Uzvārds: ");
+			u = scan.next();
+			
+			System.out.println("Telefons: ");
+			t = scan.nextInt();
+				
+					
+			
+				}
+			}
+			
+		
+		}
+
+	
+
 
 }
