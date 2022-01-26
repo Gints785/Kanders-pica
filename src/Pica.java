2239afb418ffca7fee8f4716c0d3ddff3b469094
Gints <gints.kanders@inbox.lv>
2022-01-26 23:09:48 +0200

    Programa merces

--- src/Pica.java
+++ src/Pica.java
@@ -43,6 +43,131 @@ public class Pica {
 }
 }
 //===========================================================================	
+	public static void PCDA3() {
+		Scanner scan = new Scanner(System.in);
+		String izvele;
+try {
+			
+			boolean SE = false,KE = false,DE = false,EE = false;
+			System.out.println("Picas mērce");
+	
+			ierakstit("\nPicas merce:");
+			System.out.println("1 - kečups|2 - majonēze|3 - gruķu mērce|4 - Turpinat");
+			do {
+			izvele = scan.next();		
+			izvele = izvele.toLowerCase();
+				switch(izvele){
+				case"1":
+					if(!SE) {
+					ierakstit("\nkecups");
+					SE = true;
+				}else {
+					System.out.println("šī mērce jau ir pielikts!");
+				}
+				break;
+				//==================	
+				case"2":
+					if(!KE) {
+						ierakstit("\nmajoneze");
+						KE = true;	
+				}else {
+					System.out.println("šī mērce jau ir pielikts!");
+				}
+				break;
+				//==================		
+				case"3":
+					if(!DE) {
+					ierakstit("\ngruķu mērce");
+					DE = true;
+				}else {
+					System.out.println("šī mērce jau ir pielikts!");
+				}
+				break;
+				//==================	
+				case"4":
+				
+				break;
+				//==================	
+					
+				default:System.out.println("");}
+			
+			}while(!izvele.equals("4"));
+	}catch(Exception e){
+	}
+				
+					
+
+}	
+	
+//===========================================================================	
+	public static void PCDA2(){
+		Scanner scan = new Scanner(System.in);
+		String izvele;
+		try {
+			
+			boolean SE = false,KE = false,DE = false,EE = false;
+			System.out.println("Picas piedevas");
+	
+			ierakstit("\nPicas piedevas:");
+			System.out.println("1 - siers|2 - šķinķis|3 - desa|4 - sēnes|5 - Turpinat");
+			
+			     
+			do {
+			izvele = scan.next();		
+			izvele = izvele.toLowerCase();
+				switch(izvele){
+				case"1":
+					if(!SE) {
+					ierakstit("\nsiers");
+					SE = true;
+				}else {
+					System.out.println("Siers jau ir pielikts!");
+				}
+				break;
+				//==================
+				
+				case"2":
+					if(!KE) {
+						ierakstit("\nskinkis");
+						KE = true;	
+				}else {
+					System.out.println("desa jau ir pielikts!");
+				}
+				break;
+				//==================		
+				case"3":
+					if(!DE) {
+					ierakstit("\ndesa");
+					DE = true;
+				}else {
+					System.out.println("desa jau ir pielikts!");
+				}
+				break;
+				//==================	
+				case"4":
+					if(!EE) {
+					ierakstit("\nsenes");
+					EE = true;
+				}else {
+					System.out.println("senes jau ir pielikts!");
+				}
+				break;
+				//==================	
+				case"5":
+					
+				break;
+				//==================	
+					
+				default:System.out.println("");}
+			
+			}while(!izvele.equals("5"));
+			
+			PCDA3();
+	}catch(Exception e){
+	}
+}
+		
+//===========================================================================	
 		public static void PCDA() {
 			Scanner scan = new Scanner(System.in);
 			String izvele;
@@ -57,85 +182,23 @@ public class Pica {
 				izvele = izvele.toLowerCase();
 		
 					switch(izvele){
-					case"1":ierakstit("\n30cm");
+					case"1":ierakstit("\n30cm - 2.00 Euro");
 					break;
 					//==================	
-					case"2":ierakstit("\n50cm");
+					case"2":ierakstit("\n50cm -  3.50 Euro");
 					break;
 					//==================		
 					default:System.out.println("Darbiba nepastav!");}	
 			}catch(Exception e){
 	}
+			PCDA2();
 			
-			
-			
-			try {
-				
-				boolean SE = false,KE = false,DE = false,EE = false;
-				System.out.println("Picas piedevas");
+}	
+//===========================================================================	
 		
-				ierakstit("\nPicas piedevas:");
-				System.out.println("1 - siers|2 - šķinķis|3 - desa|4 - sēnes|5- Turpinat");
-				do {
-				izvele = scan.next();		
-				izvele = izvele.toLowerCase();
-				
-					switch(izvele){
-					
-					case"1":
-						if(!SE) {
-						ierakstit("\nsiers");
-						SE = true;
-					}else {
-						System.out.println("Siers jau ir pielikts!");
-					}
-					break;
-					//==================	
-					case"2":
-						if(!KE) {
-							ierakstit("\nskinkis");
-							KE = true;	
-					}else {
-						System.out.println("desa jau ir pielikts!");
-					}
-					break;
-					//==================		
-					case"3":
-						if(!DE) {
-						ierakstit("\ndesa");
-						DE = true;
-					}else {
-						System.out.println("desa jau ir pielikts!");
-					}
-					break;
-					//==================	
-					case"4":
-						if(!EE) {
-						ierakstit("\nsenes");
-						EE = true;
-					}else {
-						System.out.println("senes jau ir pielikts!");
-					}
-					break;
-					//==================	
-					case"5":
-					
-					break;
-					//==================	
-						
-					default:System.out.println("");}
-				
-				}while(izvele=="5");
-				System.out.println("Picas");
-		}catch(Exception e){
-		}
-		System.out.println("Picas mērce");
 			
-				
 		
 
-}
-//===========================================================================		
 		public static void KLDA() {
 			Scanner scan = new Scanner(System.in);
 			String v,u,a = null;
