b3bbee5a40fc53ef0cf1a894cb7099dd13feb5af
Gints <gints.kanders@inbox.lv>
2022-01-26 20:05:11 +0200

    Switch

--- src/Pica.java
+++ src/Pica.java
@@ -3,18 +3,34 @@ import java.util.Scanner;
 public class Pica {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String izvele;
+		
+		do{
+			System.out.println("1 - Ievadīt klienta datus|2 - Ievietot faila| Atcelt");;
+			izvele = scan.next();		
+			izvele = izvele.toLowerCase();
 	
-		fikset();
+				switch(izvele){
+				case"1":
+					KLDA();
+				break;
+				//==================	
+				case"2":
+					
+				break;
+				//==================		
+				case"atcelt":
+					System.out.println("Programma aptureta!");
+				break;
+				//==================	
+				default:
+					System.out.println("Darbiba nepastav!");
+				}
+	}while(!izvele.equals("atcelt"));
+		}
 		
-		
-		
-		
-		
-		
-		
-	}
-		
-		public static void fikset() {
+		public static void KLDA() {
 			Scanner scan = new Scanner(System.in);
 			String v,u,a = null;
 			int t=0,count = 0;
@@ -25,8 +41,8 @@ public class Pica {
 			a = scan.nextLine();
 			for(int i=0;i<a.length();i++) {
 				if(Character.isDigit(a.charAt(i))) {
-					n=a.charAt(i);
-					
+				n=a.charAt(i);		
+				
 			System.out.println("vārds: ");
 			v = scan.next();
 			System.out.println("Uzvārds: ");
@@ -35,22 +51,14 @@ public class Pica {
 				count=0;
 			System.out.println("Telefons: ");
 			t = scan.nextInt();
-			
 			while (t != 0) {  
 			      t /= 10;
 			      ++count;
-			    
-			    }
+			}
 			}while(8!=count);	
 			break;	
 			
 				}
 			}
-			
-		
 		}
-
-	
-
-
-}
+	}
